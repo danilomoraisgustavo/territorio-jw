4f9f2103a857dec2907560e03ad8f8b5a8307ca1
PyDenTech <pydentech@gmail.com>
2025-06-10 20:51:40 -0300

    atualizacao

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -5,7 +5,7 @@
     .then(data => {
       document.getElementById('welcome-name').textContent = data.username;
       document.getElementById('user-email').textContent  = data.email;
-      document.getElementById('user-name').textContent = data.designacao;
+      document.getElementById('designacao').textContent = data.designacao;
     })
     .catch(console.error);
 
